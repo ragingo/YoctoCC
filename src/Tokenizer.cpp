4f480d8870286af9ab3ad5c47091c42c8ea472e3
ragingo <4784032+ragingo@users.noreply.github.com>
2026-01-11 21:33:43 +0900

    tokenizer をリファクタ

--- src/Tokenizer.cpp
+++ src/Tokenizer.cpp
@@ -11,19 +11,119 @@
 
 using namespace std::string_view_literals;
 
+namespace {
+    using namespace yoctocc;
+
+    struct ParseContext {
+        const std::string::const_iterator begin;
+        const std::string::const_iterator end;
+        std::string::const_iterator& it;
+    };
+
+    inline bool hasNext(const ParseContext& context) {
+        return std::next(context.it) != context.end;
+    }
+
+    std::shared_ptr<Token> parseNumber(ParseContext& context) {
+        std::string number;
+        while (context.it != context.end && std::isdigit(*context.it)) {
+            number += *context.it;
+            ++context.it;
+        }
+        auto token = std::make_shared<Token>(TokenKind::DIGIT);
+        token->originalValue = number;
+        token->numberValue = std::stoi(number);
+        token->location = std::distance(context.begin, context.it);
+        return token;
+    }
+
+    std::shared_ptr<Token> parseStringLiteral(ParseContext& context) {
+        std::string str;
+        ++context.it; // 最初の " をスキップ
+
+        while (context.it != context.end && *context.it != '"') {
+            if (*context.it == '\n' || *context.it == '\r' || *context.it == '\0') {
+                Log::error(std::distance(context.begin, context.it), "unclosed string literal"sv);
+                return nullptr;
+            }
+            // escape sequences
+            if (*context.it == '\\') {
+                ++context.it;
+                switch (*context.it) {
+                    case 'n': str += '\n'; break;
+                    case 't': str += '\t'; break;
+                    case 'r': str += '\r'; break;
+                    case 'a': str += '\a'; break;
+                    case 'b': str += '\b'; break;
+                    case 'f': str += '\f'; break;
+                    case 'v': str += '\v'; break;
+                    case 'e': str += 27; break;
+                    default: str += *context.it; break;
+                }
+            } else {
+                str += *context.it;
+            }
+            ++context.it;
+        }
+        ++context.it;
+
+        auto token = std::make_shared<Token>(TokenKind::STRING);
+        token->type = type::arrayOf(type::charType(), str.size() + 1);
+        token->originalValue = str;
+        token->location = std::distance(context.begin, context.it);
+        return token;
+    }
+
+    std::shared_ptr<Token> parseIdentifier(ParseContext& context) {
+        std::string identifier;
+        while (context.it != context.end && isIdentifierChar(*context.it, false)) {
+            identifier += *context.it;
+            ++context.it;
+        }
+        auto token = std::make_shared<Token>(TokenKind::IDENTIFIER);
+        token->originalValue = identifier;
+        token->location = std::distance(context.begin, context.it);
+        return token;
+    }
+
+    std::shared_ptr<Token> parsePunctuator(char ch, ParseContext& context) {
+        auto nextCh = hasNext(context) ? *std::next(context.it) : '\0';
+        std::array<char, 2> chars = { ch, nextCh };
+
+        if (chars == std::array{ '=', '=' } || chars == std::array{ '!', '=' } ||
+            chars == std::array{ '<', '=' } || chars == std::array{ '>', '=' }) {
+            auto token = std::make_shared<Token>(TokenKind::PUNCTUATOR);
+            token->originalValue = std::string{ ch, nextCh };
+            token->location = std::distance(context.begin, context.it);
+            context.it += 2;
+            return token;
+        }
+
+        auto token = std::make_shared<Token>(TokenKind::PUNCTUATOR);
+        token->originalValue = ch;
+        token->location = std::distance(context.begin, context.it);
+
+        ++context.it;
+
+        return token;
+    }
+}
+
 namespace yoctocc {
 
 std::shared_ptr<Token> tokenize(std::ifstream& ifs) {
     auto head = std::make_shared<Token>();
     auto current = head;
 
-    std::string number;
-
     std::string content{std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>()};
     Log::sourceCode = content;
-    auto it = content.begin();
+    auto it = content.cbegin();
+    auto hasNext = [&it, &content]() { return std::next(it) != content.cend(); };
+    auto isEOF = [&it, &content]() { return it == content.cend(); };
+    auto startLocation = [&it, &content]() { return std::distance(content.cbegin(), it); };
+    ParseContext context{ content.cbegin(), content.cend(), it };
 
-    while (it != content.end()) {
+    while (it != content.cend()) {
         char ch = *it;
 
         if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r') {
@@ -31,106 +131,27 @@ std::shared_ptr<Token> tokenize(std::ifstream& ifs) {
             continue;
         }
 
+        std::shared_ptr<Token> next;
+
         if (std::isdigit(ch)) {
-            number += ch;
-
-            if (std::next(it) != content.end()) {
-                ++it;
-                continue;
-            }
+            next = parseNumber(context);
+        } else if (ch == '"') {
+            next = parseStringLiteral(context);
+        } else if (isIdentifierChar(ch, true)) {
+            next = parseIdentifier(context);
+        } else if (std::ispunct(ch)) {
+            next = parsePunctuator(ch, context);
+        } else {
+            Log::error(startLocation(), std::format("Unexpected character '{}'", ch));
+            return nullptr;
         }
 
-        if (!number.empty()) {
-            auto next = std::make_shared<Token>(TokenKind::DIGIT);
-            next->originalValue = number;
-            next->numberValue = std::stoi(number);
-            next->location = std::distance(content.begin(), it) - number.size();
-            current->next = next;
-            current = next;
-            number.clear();
+        if (!next) {
+            return nullptr;
         }
 
-        // string literal
-        if (ch == '"') {
-            std::string str;
-            size_t startLocation = std::distance(content.begin(), it);
-            ++it;
-            while (it != content.end() && *it != '"') {
-                if (*it == '\n' || *it == '\r' || *it == '\0') {
-                    Log::error(std::distance(content.begin(), it), "unclosed string literal"sv);
-                    return nullptr;
-                }
-                // escape sequences
-                if (*it == '\\') {
-                    ++it;
-                    switch (*it) {
-                        case 'n': str += '\n'; break;
-                        case 't': str += '\t'; break;
-                        case 'r': str += '\r'; break;
-                        case 'a': str += '\a'; break;
-                        case 'b': str += '\b'; break;
-                        case 'f': str += '\f'; break;
-                        case 'v': str += '\v'; break;
-                        case 'e': str += 27; break;
-                        default: str += *it; break;
-                    }
-                } else {
-                    str += *it;
-                }
-                ++it;
-            }
-            ++it;
-
-            auto next = std::make_shared<Token>(TokenKind::STRING);
-            next->type = type::arrayOf(type::charType(), str.size() + 1);
-            next->originalValue = str;
-            next->location = startLocation;
-            current->next = next;
-            current = next;
-            continue;
-        }
-
-        if (isIdentifierChar(ch, true)) {
-            std::string identifier;
-            identifier += ch;
-            ++it;
-            while (it != content.end() && isIdentifierChar(*it, false)) {
-                identifier += *it;
-                ++it;
-            }
-            auto next = std::make_shared<Token>(TokenKind::IDENTIFIER);
-            next->originalValue = identifier;
-            next->location = std::distance(content.begin(), it) - identifier.size();
-            current->next = next;
-            current = next;
-            continue;
-        }
-
-        if (std::ispunct(ch)) {
-            auto nextCh = std::next(it) != content.end() ? *std::next(it) : '\0';
-            std::array<char, 2> chars = { ch, nextCh };
-            if (chars == std::array{ '=', '=' } || chars == std::array{ '!', '=' } ||
-                chars == std::array{ '<', '=' } || chars == std::array{ '>', '=' }) {
-                auto next = std::make_shared<Token>(TokenKind::PUNCTUATOR);
-                next->originalValue = std::string{ ch, nextCh };
-                next->location = std::distance(content.begin(), it);
-                current->next = next;
-                current = next;
-                it += 2;
-                continue;
-            }
-
-            auto next = std::make_shared<Token>(TokenKind::PUNCTUATOR);
-            next->originalValue = ch;
-            next->location = std::distance(content.begin(), it);
-            current->next = next;
-            current = next;
-            ++it;
-            continue;
-        }
-
-        Log::error(std::distance(content.begin(), it), std::format("Unexpected character '{}'", ch));
-        return nullptr;
+        current->next = next;
+        current = next;
     }
 
     auto terminator = std::make_shared<Token>(TokenKind::TERMINATOR);
